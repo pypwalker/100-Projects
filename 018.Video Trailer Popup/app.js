1dbfe96fa722969e4165d4399815bf7e0ee342b9
Aung Paing Htoo <pypdaywalker999@gmail.com>
2024-08-08 08:27:25 +0630

    Video Trailer Popup

---
+++ 018.Video Trailer Popup/app.js
@@ -0,0 +1,27 @@
+// btn
+const btn = document.querySelector('.btn');
+
+// close-icon
+const closeIcon = document.querySelector(".close-icon");
+
+// trailer-container
+const trailerContainer = document.querySelector('.trailer-container');
+
+// Video
+const video = document.querySelector('video');
+
+// console.log(btn);
+// console.log(closeIcon);
+// console.log(trailerContainer);
+// console.log(video);
+
+btn.addEventListener('click', () => {
+    trailerContainer.classList.remove("active");
+});
+
+
+closeIcon.addEventListener('click', () => {
+    trailerContainer.classList.add('active');
+    video.pause();
+    video.currentTime = 0;
+});
