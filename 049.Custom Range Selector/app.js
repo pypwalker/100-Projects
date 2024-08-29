c1776187f5eeb33aa532390821a3c7b05d5a9a76
Aung Paing Htoo <pypdaywalker999@gmail.com>
2024-08-29 16:35:56 +0630

    +meaning

--- 049.Custom Range Selector/app.js
+++ 049.Custom Range Selector/app.js
@@ -1,21 +1,29 @@
 const range = document.getElementById("range");
 
+ // + for Conversion to a Number
+
 range.addEventListener('input', (e) => {
-    const value = +e.target.value;
+    const value = +e.target.value;             
+    console.log(value);                         
     const label = e.target.nextElementSibling;
 
     const range_width = getComputedStyle(e.target).getPropertyValue('width');
+
     const label_width = getComputedStyle(label).getPropertyValue("width");
 
-    const num_width = +range_width.substring(0, range_width.length - 2);
+    const num_width = +range_width.substring(0, range_width.length - 2); 
+
     const num_label_width = +label_width.substring(0, label_width.length - 10);
 
+
     const max = +e.target.max;
+
     const min = +e.target.min;
+
 
     const left = 
                 value * (num_width / max) - 
-                num_label_width /2 + 
+                num_label_width / 2 + 
                 scale(value, min, max, 10, -10);
 
     label.style.left = `${left}px`
