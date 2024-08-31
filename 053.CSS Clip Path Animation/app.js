cc0d082b7d915bd134fea66010bd16690d91c9c8
Aung Paing Htoo <pypdaywalker999@gmail.com>
2024-08-31 08:37:51 +0630

    css clip-path animation added

---
+++ 053.CSS Clip Path Animation/app.js
@@ -0,0 +1,7 @@
+let pos = document.documentElement
+
+pos.addEventListener('mousemove', e => {
+    pos.style.setProperty("--1", e.clientX + "px");
+    pos.style.setProperty("--2", e.clientY + "px");
+
+});
