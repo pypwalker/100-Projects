adaf682ce90e71ceb932d0ec1ffbda9f935c2683
Aung Paing Htoo <pypdaywalker999@gmail.com>
2024-09-08 20:07:06 +0630

    img_links updated & bug_fixed

--- 079.Testimonal Box Switcher/app.js
+++ 079.Testimonal Box Switcher/app.js
@@ -16,42 +16,42 @@ const testimonials = [
       name: "Dacey Nolan",
       position: "@dacey_nolan",
       photo:
-        "https://pbs.twimg.com/profile_images/1356685491127656449/db8jKmuZ_400x400.jpg",
+        "https://images.unsplash.com/photo-1573496359142-b8d87734a5a2?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8ZGV2ZWxvcGVyJTIwcHJvZmlsZXxlbnwwfHwwfHx8MA%3D%3D",
       text: "I started using @tailwindcss. I instantly fell in love with their responsive modifiers, thorough documentation, and how easy it was customizing color palettes.",
     },
     {
       name: "Sarah Dayan",
       position: "@frontstuff_io",
       photo:
-        "https://pbs.twimg.com/profile_images/977873484759158784/mOItIR7M_400x400.jpg",
+        "https://plus.unsplash.com/premium_photo-1689708721750-8a0e6dc14cee?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8ZGV2ZWxvcGVyJTIwcHJvZmlsZXxlbnwwfHwwfHx8MA%3D%3D",
       text: "Tailwind CSS is bridging the gap between design systems and products. It's becoming the defacto API for styling.",
     },
     {
       name: "Igor Randjelovic",
       position: "@igor_randj",
       photo:
-        "https://pbs.twimg.com/profile_images/970109919444824064/X0XU8ZD9_400x400.jpg",
+        "https://plus.unsplash.com/premium_photo-1689570350306-3aa2bc42189e?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8ZGV2ZWxvcGVyJTIwcHJvZmlsZXxlbnwwfHwwfHx8MA%3D%3D",
       text: "Tailwind clicked for me almost immediately. I can't picture myself writing another BEM class ever again. Happy user since the first public release! Productivity is at an all time high, thanks to @tailwindcss",
     },
     {
       name: "Dan Malone",
       position: "@ohhdanm",
       photo:
-        "https://pbs.twimg.com/profile_images/1523786296308736000/aJ7nC2LN_400x400.jpg",
+        "https://images.unsplash.com/photo-1573497019940-1c28c88b4f3e?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fGRldmVsb3BlciUyMHByb2ZpbGV8ZW58MHx8MHx8fDA%3D",
       text: "CSS has always been the hardest part of offering a digital service. It made me feel like a bad developer. Tailwind gives me confidence in web development again. Their docs are my first port of call.",
     },
     {
       name: "Sergio Peris",
       position: "@sertxudev",
       photo:
-        "https://pbs.twimg.com/profile_images/1526657447326371842/vtmVANH7_400x400.jpg",
+        "https://images.unsplash.com/photo-1568602471122-7832951cc4c5?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDN8fHxlbnwwfHx8fHw%3D",
       text: "I thought 'Why would I need Tailwind CSS? I already know CSS and use Bootstrap', but after giving it a try I decided to switch all my projects to Tailwind.",
     },
     {
       name: "marckohlbrugge.eth",
       position: "@marckohlbrugge",
       photo:
-        "https://pbs.twimg.com/profile_images/1517414077534191616/fUc3KRh6_400x400.jpg",
+        "https://images.unsplash.com/photo-1560250097-0b93528c311a?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDJ8fHxlbnwwfHx8fHw%3D",
       text: "Before Tailwind CSS I was banging my head against the wall trying to make sense of my CSS spaghetti. Now I am banging my head against the wall wondering why I didn't try it earlier. My head hurts and my wall has a big hole in it. But at least using CSS is pleasant again!",
     },
   ];
@@ -68,8 +68,8 @@ const testimonials = [
 
     counter++
 
-    if (counter > testimonial.length - 1) {
-        counter = 0
+    if (counter > testimonials.length - 1) {
+        counter = 0;
     }
   }
 
